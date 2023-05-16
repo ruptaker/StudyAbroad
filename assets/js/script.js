78517374eb786a4bcb9a18627c803a1f8ef53011
Rupesh Shrestha <63822477+ruptaker@users.noreply.github.com>
2023-05-16 16:41:25 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,6 +59,13 @@ const activeElem = function () {
     backTopBtn.classList.remove("active");
   }
 }
+
+function slider(input) {
+  const item = document.querySelector(`#${input}`);
+  const slideArr = [...document.querySelector(".slides").children];
+  slideArr.forEach((element) => element.classList.remove("active"));
+  item.classList.add("active");
+}
 let slideIndex = 1;
 setInterval(() => {
   slideIndex++;
