fdaebcbfa0a47f2e80e19730107f4d39db672348
Rupesh Shrestha <63822477+ruptaker@users.noreply.github.com>
2024-02-05 19:50:01 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,74 +3,64 @@
 
 
 /**
- * add event on element
+ * add eventListener on multiple elements
  */
 
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
-    }
-  } else {
-    elem.addEventListener(type, callback);
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
   }
 }
 
 
 
 /**
- * navbar toggle
+ * PRELOADER
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+const circle = document.querySelector("[data-circle]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  circle.style.animation = "none";
+  document.body.classList.add("loaded");
+});
+
+
+
+/**
+ * NAVBAR TOGGLER FOR MOBILE
  */
 
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-const navLinks = document.querySelectorAll("[data-nav-link]");
 const overlay = document.querySelector("[data-overlay]");
 
 const toggleNavbar = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
 }
 
-addEventOnElem(navTogglers, "click", toggleNavbar);
-
-const closeNavbar = function () {
-  navbar.classList.remove("active");
-  overlay.classList.remove("active");
-}
-
-addEventOnElem(navLinks, "click", closeNavbar);
+addEventOnElements(navTogglers, "click", toggleNavbar);
 
 
 
 /**
- * header active when scroll down to 100px
+ * HEADER
+ * 
+ * add active class on header when window scroll down to 100px
  */
 
 const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
 
-const activeElem = function () {
+const headerActive = function () {
   if (window.scrollY > 100) {
     header.classList.add("active");
-    backTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
-    backTopBtn.classList.remove("active");
   }
 }
 
-function slider(input) {
-  const item = document.querySelector(`#${input}`);
-  const slideArr = [...document.querySelector(".slides").children];
-  slideArr.forEach((element) => element.classList.remove("active"));
-  item.classList.add("active");
-}
-let slideIndex = 1;
-setInterval(() => {
-  slideIndex++;
-  if (slideIndex === 7) slideIndex = 1;
-  slider(`slide${slideIndex}`);
-}, 2000);
-
-addEventOnElem(window, "scroll", activeElem);
+window.addEventListener("scroll", headerActive);
